e175cdf964ca0b71ec8cb7b9bb039ea9c318bf7c
Tobi Ogunsola <tomobi552@gmail.com>
2024-03-26 21:39:08 +0100

    reactstate checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    person: {
+      fullName: 'Femi Babatolu',
+      bio: 'A full mtime software engineer that has 5 years experience in the field.',
+      imgSrc: 'https://media.istockphoto.com/id/1450969748/photo/developer-working-with-new-program.jpg?s=2048x2048&w=is&k=20&c=iFBySg9gYWU20rRPhwafcTBroJB_0qCBuotH2BcPQGs=',
+      profession: 'Software Engineer'
+    },
+    shows: false,
+    mountTime: null
+  };
+
+  componentDidMount() {
+    this.setState({ mountTime: new Date() });
+  }
+
+  toggleShow = () => {
+    this.setState(prevState => ({
+      shows: !prevState.shows
+    }));
+  };
+
+  render() {
+    const { person, shows, mountTime } = this.state;
+    const profile = (
+      <div>
+        <h2>{person.fullName}</h2>
+        <p>{person.bio}</p>
+        <img src={person.imgSrc} alt="profile" />
+        <p>{person.profession}</p>
+      </div>
+    );
+
+    return (
+      <div>
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+        {shows && profile}
+        {mountTime && (
+          <p>
+            Component mounted {Math.floor((new Date() - mountTime) / 1000)} seconds ago.
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
